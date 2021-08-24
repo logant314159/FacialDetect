f7f166ff2572c889d1b80ec99aece09c5e8d150e
Logan Thompson <logant314159@gmail.com>
2021-08-24 16:50:22 -0500

    Moved some functionality to the ParseFrame method.

--- FacialDetect.py
+++ FacialDetect.py
@@ -5,12 +5,16 @@ import numpy as np
 cascade = cv.CascadeClassifier();
 cascade.load("faceCascade.xml")
 
-def GetFaceCoords(frame):
-    frameArray = np.fromstring(frame, np.uint8)
-    frame = cv.imdecode(frameArray, cv.IMREAD_COLOR)
+def ParseFrame(image):
+    if type(image) == str:
+        with open(image, "r") as file:
+            frameArray = np.fromstring(file, np.uint8)
+        
+        return cv.imdecode(frameArray, cv.IMREAD_COLOR)
 
+
+def GetFaceCoords(frame):
     grayFrame = cv.equalizeHist(cv.cvtColor(frame, cv.COLOR_BGR2GRAY))
 
     return cascade.detectMultiScale(grayFrame)
-
 
